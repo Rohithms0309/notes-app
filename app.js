78f3b4246b2aba8670b15e6170a9158524003f62
Rohith <rohithbrms555@gmail.com>
2023-05-04 01:21:19 +0530

    Added yargs

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// Customize yargs version
 
-const greenMsg = chalk.blue.bgRed.bold('Error!')
-console.log(greenMsg);
+yargs.version('1.1.0')
+
+
+// add, remove, read, list notes
+
+// Create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+
+})
+
+// Create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Remove the note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes!')
+    }
+})
+
+// Read the notes
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function () {
+        console.log('Read the notes!')
+    }
+})
+
+console.log(yargs.argv)
